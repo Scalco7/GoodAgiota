dd9e0db07baa159722cc968426e6d8fd29b1326a
Felipe Scalco <fscalco7@gmail.com>
2025-11-01 20:52:12 -0300

    add fonts: inter and poppins

--- frontend/web/src/app/app.config.ts
+++ frontend/web/src/app/app.config.ts
@@ -4,6 +4,7 @@ import { providePrimeNG } from 'primeng/config';
 import { Agio } from './shared/css-theme/agio.preset';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
