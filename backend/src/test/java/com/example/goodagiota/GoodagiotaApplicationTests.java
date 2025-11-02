910ee6cfa63ee83bf4eeabc90cfd7ec9572bca15
Felipe Scalco <fscalco7@gmail.com>
2025-11-02 10:37:21 -0300

    init spring boot api

---
+++ backend/src/test/java/com/example/goodagiota/GoodagiotaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goodagiota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodagiotaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
