a6ee22f2786645b915e745ea45f286cfddd23491
Felipe Scalco <fscalco7@gmail.com>
2025-11-02 16:54:08 -0300

    feat [API]: add repositories

---
+++ backend/src/main/java/com/example/goodagiota/repositories/LoanRepository.java
@@ -0,0 +1,12 @@
+package com.example.goodagiota.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.goodagiota.entities.Loan;
+
+@Repository
+public interface LoanRepository extends JpaRepository<Loan, String> {
+    
+}
+
